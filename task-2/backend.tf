6aee3b4dfa8ef9344335b94b12a9ee6c2c8df8e3
yorqin <yorqin@yorqin.yorqin>
2024-10-11 21:40:07 +0500

    Configuration ami and set up default folder

--- task-2/backend.tf
+++ task-2/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket         = "terraform-backend-aws-task2"
+        bucket         = "terraform-backend-aws-devops"
         key            =  "state/terraform_state.tfstate"
         region         = "ap-south-1"
         encrypt        = true                   
--- task-2/variables.tf
+++ task-2/variables.tf
@@ -3,6 +3,8 @@ variable "region" {
   default     = "ap-south-1"
 }
 
+
+
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   default     = "10.0.0.0/16"
